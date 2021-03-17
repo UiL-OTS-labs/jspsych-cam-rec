93c80b936aee1758e82ab89da01b3e475bd184d4
Jacco van Elst <j.c.vanelst@uu.nl>
2021-03-17 13:15:33 +0100

    in between commit, let's go to the basement!

--- jspsych/stimuli.js
+++ jspsych/stimuli.js
@@ -3,12 +3,20 @@
 ///////////////
 
 // Item types
-const NON_WORD = "NON_WORD";
-const REAL_WORD = "REAL_WORD";
-const PRACTICE = "PRACTICE";
+const GRABBER = "GRABBER";
+
+const BASELINE = "BASELINE";
+const PAIRED_SUCCESION = "PAIRED_SUCCESION";
+
+// Subtypes Image /Sound
+const SIMPLER = "SIMPLER";
+const COMPLEX = "COMPLEX";
+
+const SOUND_1 = "SOUND_1";
+const SOUND_2 = "SOUND_2";
 
 // name for lists, in this case one list, one item in the list
-const LISTS = ["my_one_and_only_list"];
+const LISTS = ["list_one"];
 
 // In case of more complex design, the above could be, for example:
 
@@ -17,32 +25,101 @@ const LISTS = ["my_one_and_only_list"];
 //     "my_second_list"
 // ];
 
-const PRACTICE_ITEMS = [
-    { id: 1, item_type: PRACTICE, word: "palve", filename: "./sounds/palve.wav", correct: 0 },
-    { id: 2, item_type: PRACTICE, word: "hot", filename: "./sounds/hot.wav", correct: 1 }
+const BASELINE = [
+    {
+        id: 1, 
+        item_type: BASELINE, 
+        sound_fn_a: "./sounds/baseline_one.wav",
+        image_fn_a: "./images/baseline_a",
+        image_fn_b: "./images/baseline_b",    
+    },
+    {
+        id: 2, 
+        item_type: BASELINE, 
+        sound_fn_a: "./sounds/baseline_one.wav",
+        image_fn_a: "./images/baseline_a",
+        image_fn_b: "./images/baseline_b",    
+    },
 ];
 
 const LIST_1 = [
-    { id: 1, item_type: NON_WORD, word: "slirque", filename: "./sounds/slirque.wav", correct: 0 },
-    { id: 2, item_type: NON_WORD, word: "crawse", filename: "./sounds/crawse.wav", correct: 0 },
-    { id: 3, item_type: NON_WORD, word: "thwurp", filename: "./sounds/thwurp.wav", correct: 0 },
-    { id: 4, item_type: NON_WORD, word: "clem", filename: "./sounds/clem.wav", correct: 0 },
-    { id: 5, item_type: REAL_WORD, word: "white", filename: "./sounds/white.wav", correct: 1 },
-    { id: 6, item_type: REAL_WORD, word: "travel", filename: "./sounds/travel.wav", correct: 1 },
-    { id: 7, item_type: REAL_WORD, word: "letter", filename: "./sounds/letter.wav", correct: 1 },
-    { id: 8, item_type: REAL_WORD, word: "clown", filename: "./sounds/clown.wav", correct: 1  }
+    {
+        id: 1, 
+        item_type: NON_WORD, 
+        word: "slirque", 
+        wordfn: "./sounds/slirque.wav",
+        prime: "eyes",
+        correct: 0
+    },
+    {
+        id: 2, 
+        item_type: NON_WORD, 
+        word: "crawse", 
+        wordfn: "./sounds/crawse.wav",
+        prime: "piano",
+        correct: 0
+    },
+    {
+        id: 3, 
+        item_type: NON_WORD, 
+        word: "thwurp", 
+        wordfn: "./sounds/thwurp.wav",
+        prime: "rabbit",
+        correct:0
+    },
+    {
+        id: 4, 
+        item_type: NON_WORD, 
+        word: "clem", 
+        wordfn: "./sounds/clem.wav",
+        prime: "flower",
+        correct: 0
+    }, 
+    {
+        id: 5, 
+        item_type: RELATED, 
+        word: "white", 
+        wordfn: "./sounds/white.wav",
+        prime: "snow",
+        correct: 1
+    },
+    {
+        id: 6, 
+        item_type: RELATED, 
+        word: "travel", 
+        wordfn: "./sounds/travel.wav",
+        prime: "suitcase",
+        correct: 1
+    },
+    {
+        id: 7, 
+        item_type: UNRELATED, 
+        word: "letter", 
+        wordfn: "./sounds/letter.wav",
+        prime: "garden",
+        correct: 1
+    },
+    {
+        id: 8, 
+        item_type: UNRELATED, 
+        word: "clown", 
+        wordfn: "./sounds/clown.wav",
+        prime: "forest",
+        correct: 1
+    }
 ];
+
+
+// Add a second list of stimuli when required.
+// const LIST_2 = [
+// ...
+// ]
 
 const TEST_ITEMS = [
     {list_name: LISTS[0], table: LIST_1}
-    ];
-   
-// If there were two lists to choose from:
-
-// const TEST_ITEMS = [
-//     {list_name: LISTS[0], table: LIST_1},
-//     {list_name: LISTS[1], table: LIST_2}
-// ];
+    // Add the second list here, put a comma on the end of the line above here.
+    //{list_name: LISTS[1], table: LIST_2}
+];
 
 /**
  * Get the list of practice items
@@ -50,7 +127,7 @@ const TEST_ITEMS = [
  * Returns an object with a list and a table, the list will always indicate
  * "practice" since it are the practice items
  *
- * @returns {object} object with list and table fields
+ * @returns {object} object with group and table fields
  */
 function getPracticeItems() {
     return {list_name : "practice", table : PRACTICE_ITEMS};
@@ -58,7 +135,11 @@ function getPracticeItems() {
 
 /**
  * This function will pick a random list from the TEST_ITEMS array.
- * @returns {object} object with one or more "lists" and table fields
+ *
+ * Returns an object with a list and a table, the list will always indicate
+ * which list has been chosen for the participant.
+ *
+ * @returns {object} object with list and table fields
  */
 function pickRandomList() {
     let range = function (n) {
@@ -70,7 +151,7 @@ function pickRandomList() {
         return empty_array;
     }
     let num_lists = TEST_ITEMS.length;
-    var shuffled_range = jsPsych.randomization.repeat(range(num_lists), 1);
+    var shuffled_range = jsPsych.randomization.repeat(range(num_lists), 1)
     var retlist = TEST_ITEMS[shuffled_range[0]];
-    return retlist;
+    return retlist
 }
