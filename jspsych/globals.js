f466862ccf95d1915361cd97f40b72225b56ea6a
Jacco van Elst <j.c.vanelst@uu.nl>
2021-03-18 13:02:45 +0100

    making a start...

--- jspsych/globals.js
+++ jspsych/globals.js
@@ -11,7 +11,6 @@ const YES_BUTTON_TEXT = "Yes";
 const NO_BUTTON_TEXT = "No";
 const TRUE_BUTTON_TEXT = "True";
 const FALSE_BUTTON_TEXT = "False";
-const DEFAULT_ITI = 500;
 
 // Default restrictions of minimal browser dimensions
 const MIN_WIDTH = 800; //will become wider...
@@ -19,4 +18,13 @@ const MIN_HEIGHT = 600;
 
 // Bail out string for mobiles
 const BAIL_OUT_MOBILE_TEXT = "Please run this experiment on a PC or Laptop."
+
+// Default ITI
+const DEFAULT_ITI = 500;
+
+// Task specific meat Presentation Time (PT) Mean
+const BASELINE_MU_PT = 1950;
+const PAIRED_MU_PT = 9950;
+const GRABBER_MU_PT = 2450;
+
 
--- jspsych/stimuli.js
+++ jspsych/stimuli.js
@@ -15,143 +15,90 @@ const COMPLEX = "COMPLEX";
 const SOUND_1 = "SOUND_1";
 const SOUND_2 = "SOUND_2";
 
-// name for lists, in this case one list, one item in the list
-const LISTS = ["list_one"];
 
-// In case of more complex design, the above could be, for example:
+// Baseline items 1
+const BASELINE_1 = [
+    {
+        id: 1, 
+        item_type: BASELINE, 
+        sound_fn: "./sounds/testtune.mp3",
+        image_fn_left: "./images/simple1.png",
+        image_fn_right: "./images/simple2.png"
+    },
+    {
+        id: 2, 
+        item_type: BASELINE, 
+        sound_fn: "./sounds/testtune.mp3",
+        image_fn_left: "./images/complex1.png",
+        image_fn_right: "./images/complex2.png"
+    }
+];
 
-// const LISTS = [
-//     "my_first_list",
-//     "my_second_list"
+// // baseline items 2
+// const BASELINE_2 = [
+//     {
+//         id: 1, 
+//         item_type: BASELINE, 
+//         sound_fn: "./sounds/testtune.mp3",
+//         image_fn_left: "./images/simple2.png",
+//         image_fn_right: "./images/complex2.png"  
+//     },
+//     {
+//         id: 2, 
+//         item_type: BASELINE, 
+//         sound_fn: "./sounds/testtune.mp3",
+//         image_fn_left: "./images/complex1.png",
+//         image_fn_right: "./images/simple1.png"    
+//     }
 // ];
 
-const BASELINE = [
+// Paired succesion items
+const PAIRED_1 = [
     {
         id: 1, 
-        item_type: BASELINE, 
-        sound_fn_a: "./sounds/baseline_one.wav",
-        image_fn_a: "./images/baseline_a",
-        image_fn_b: "./images/baseline_b",    
+        item_type: PAIRED_SUCCESION, 
+        sound_fn: "./sounds/testtune.mp3",
+        image_fn_left: "./images/simple1.png",
+        image_fn_right: "./images/complex1.png"
     },
     {
         id: 2, 
-        item_type: BASELINE, 
-        sound_fn_a: "./sounds/baseline_one.wav",
-        image_fn_a: "./images/baseline_a",
-        image_fn_b: "./images/baseline_b",    
-    },
-];
-
-const LIST_1 = [
-    {
-        id: 1, 
-        item_type: NON_WORD, 
-        word: "slirque", 
-        wordfn: "./sounds/slirque.wav",
-        prime: "eyes",
-        correct: 0
-    },
-    {
-        id: 2, 
-        item_type: NON_WORD, 
-        word: "crawse", 
-        wordfn: "./sounds/crawse.wav",
-        prime: "piano",
-        correct: 0
-    },
-    {
-        id: 3, 
-        item_type: NON_WORD, 
-        word: "thwurp", 
-        wordfn: "./sounds/thwurp.wav",
-        prime: "rabbit",
-        correct:0
-    },
-    {
-        id: 4, 
-        item_type: NON_WORD, 
-        word: "clem", 
-        wordfn: "./sounds/clem.wav",
-        prime: "flower",
-        correct: 0
-    }, 
-    {
-        id: 5, 
-        item_type: RELATED, 
-        word: "white", 
-        wordfn: "./sounds/white.wav",
-        prime: "snow",
-        correct: 1
-    },
-    {
-        id: 6, 
-        item_type: RELATED, 
-        word: "travel", 
-        wordfn: "./sounds/travel.wav",
-        prime: "suitcase",
-        correct: 1
-    },
-    {
-        id: 7, 
-        item_type: UNRELATED, 
-        word: "letter", 
-        wordfn: "./sounds/letter.wav",
-        prime: "garden",
-        correct: 1
-    },
-    {
-        id: 8, 
-        item_type: UNRELATED, 
-        word: "clown", 
-        wordfn: "./sounds/clown.wav",
-        prime: "forest",
-        correct: 1
+        item_type: PAIRED_SUCCESION, 
+        sound_fn: "./sounds/testtune.mp3",
+        image_fn_left: "./images/complex3.png",
+        image_fn_right: "./images/simple3.png"   
     }
 ];
 
+// const PAIRED_2 = [
+//     {
+//         id: 1, 
+//         item_type: PAIRED_SUCCESION, 
+//         sound_fn: "./sounds/testtune.mp3",
+//         image_fn_left: "./images/simple3.png",
+//         image_fn_right: "./images/complex3.png"  
+//     },
+//     {
+//         id: 2, 
+//         item_type: PAIRED_SUCCESION, 
+//         sound_fn: "./sounds/testtune.mp3",
+//         image_fn_left: "./images/comlex1.png",
+//         image_fn_right: "./images/simple1.png",    
+//     }
+// ];
 
-// Add a second list of stimuli when required.
-// const LIST_2 = [
-// ...
-// ]
 
-const TEST_ITEMS = [
-    {list_name: LISTS[0], table: LIST_1}
-    // Add the second list here, put a comma on the end of the line above here.
-    //{list_name: LISTS[1], table: LIST_2}
-];
+// const PAIRED_ITEMS = [
+//     {list_name: LISTS[0], table: PAIRED_1},
+//     {list_name: LISTS[1], table: PAIRED_2}
+// ];
 
-/**
- * Get the list of practice items
- *
- * Returns an object with a list and a table, the list will always indicate
- * "practice" since it are the practice items
- *
- * @returns {object} object with group and table fields
- */
-function getPracticeItems() {
-    return {list_name : "practice", table : PRACTICE_ITEMS};
+
+function getBaselineItems() {
+    return {list_name : "baseline one", table : BASELINE_1};
 }
 
-/**
- * This function will pick a random list from the TEST_ITEMS array.
- *
- * Returns an object with a list and a table, the list will always indicate
- * which list has been chosen for the participant.
- *
- * @returns {object} object with list and table fields
- */
-function pickRandomList() {
-    let range = function (n) {
-        let empty_array = [];
-        let i;
-        for (i = 0; i < n; i++) {
-            empty_array.push(i);
-        }
-        return empty_array;
-    }
-    let num_lists = TEST_ITEMS.length;
-    var shuffled_range = jsPsych.randomization.repeat(range(num_lists), 1)
-    var retlist = TEST_ITEMS[shuffled_range[0]];
-    return retlist
+function getPairedItems() {
+    return {list_name : "paired one", table : PAIRED_1};
 }
+
