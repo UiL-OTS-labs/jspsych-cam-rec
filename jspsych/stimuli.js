a3139fe29b52547861fe24acbbe3ce0e335f1c31
Jacco van Elst <j.c.vanelst@uu.nl>
2021-03-24 10:27:17 +0100

    dummy squares, should match BG color + table color

--- jspsych/stimuli.js
+++ jspsych/stimuli.js
@@ -24,37 +24,54 @@ const BASELINE_1 = [
         id: 1, 
         item_type: BASELINE, 
         sound_fn: "./sounds/testtune.mp3",
-        image_fn_left: "./images/circle_complex_movement.gif",
-        image_fn_right: "./images/circle_simple_movement.gif"
+        image_fn_left: "./images/complex1.png",
+        image_fn_right: "./images/complex1.png"
     },
     {
         id: 2, 
         item_type: BASELINE, 
         sound_fn: "./sounds/testtune.mp3",
-        image_fn_left: "./images/circle_simple_movement.gif",
-        image_fn_right: "./images/circle_complex_movement.gif"
+        image_fn_left: "./images/simple1.png",
+        image_fn_right: "./images/simple2.png",
     }
 ];
 
 
 // Paired succesion items
+//side can be 0 or 1 (0: left, 1: right)
 const PAIRED_1 = [
     {
         id: 1, 
         item_type: PAIRED_SUCCESION, 
         sound_fn: "./sounds/testtune.mp3",
-        image_fn_left: "./images/circle_simple_movement.gif",
-        image_fn_right: "./images/circle_simple_movement.gif"
+        image_fn: "./images/simple1.png",
+        side: 0 
     },
     {
         id: 2, 
         item_type: PAIRED_SUCCESION, 
         sound_fn: "./sounds/testtune.mp3",
-        image_fn_left: "./images/circle_complex_movement.gif",
-        image_fn_right: "./images/circle_complex_movement.gif"   
+        image_fn: "./images/simple1.png",
+        side: 1
     }
 ];
 
+const PAIRED_2 = [
+    {
+        id: 1, 
+        item_type: PAIRED_SUCCESION, 
+        sound_fn: "./sounds/testtune.mp3",
+        image_fn: "./images/simple1.png",
+        side: 0 
+    },
+    {
+        id: 2, 
+        item_type: PAIRED_SUCCESION, 
+        sound_fn: "./sounds/testtune.mp3",
+        image_fn: "./images/simple1.png",
+        side: 1
+    }
+];
 
 
 // var image_size = [200,200] // pixels
@@ -67,7 +84,7 @@ const PAIRED_1 = [
 
 
 // table layout
-GRID_HTML = `<div id="jspsych-vsl-grid-scene-dummy" css="display: none;">
+GRID_HTML_LEFT_RIGHT = `<div id="jspsych-vsl-grid-scene-dummy" css="display: none;">
     <table id="jspsych-vsl-grid-scene table" style="border-collapse: collapse; margin-left: auto; margin-right: auto;">
     <tr id="jspsych-vsl-grid-scene-table-row-0" css="height: 200px;">
     <td id="jspsych-vsl-grid-scene-table-0-0" style="padding: 20px 20px; border: 1px solid white;">
@@ -107,6 +124,44 @@ GRID_HTML = `<div id="jspsych-vsl-grid-scene-dummy" css="display: none;">
     </td></tr></table></div>`
 
 
+GRID_HTML_CENTER = `<div id="jspsych-vsl-grid-scene-dummy" css="display: none;">
+    <table id="jspsych-vsl-grid-scene table" style="border-collapse: collapse; margin-left: auto; margin-right: auto;">
+    <tr id="jspsych-vsl-grid-scene-table-row-0" css="height: 200px;">
+    <td id="jspsych-vsl-grid-scene-table-0-0" style="padding: 20px 20px; border: 1px solid white;">
+    <div id="jspsych-vsl-grid-scene-table-cell-0-0" style="width: 200px; height: 200px;"></div></td>
+    <td id="jspsych-vsl-grid-scene-table-0-1" style="padding: 20px 20px; border: 1px solid white;">
+    <div id="jspsych-vsl-grid-scene-table-cell-0-1" style="width: 200px; height: 200px;"></div>
+    </td><td id="jspsych-vsl-grid-scene-table-0-2" style="padding: 20px 20px; border: 1px solid white;">
+    <div id="jspsych-vsl-grid-scene-table-cell-0-2" style="width: 200px; height: 200px;">
+    </div></td><td id="jspsych-vsl-grid-scene-table-0-3" style="padding: 20px 20px; border: 1px solid white;">
+    <div id="jspsych-vsl-grid-scene-table-cell-0-3" style="width: 200px; height: 200px;"></div>
+    </td><td id="jspsych-vsl-grid-scene-table-0-4" style="padding: 20px 20px; border: 1px solid white;">
+    <div id="jspsych-vsl-grid-scene-table-cell-0-4" style="width: 200px; height: 200px;"></div>
+    </td></tr><tr id="jspsych-vsl-grid-scene-table-row-1" css="height: 200px;">
+    <td id="jspsych-vsl-grid-scene-table-1-0" style="padding: 20px 20px; border: 1px solid white;">
+    <div id="jspsych-vsl-grid-scene-table-cell-1-0" style="width: 200px; height: 200px;">
+    <img src="./images/left.png" style="width: 200px; height: 200"></img></div>
+    </td><td id="jspsych-vsl-grid-scene-table-1-1" style="padding: 20px 20px; border: 1px solid white;">
+    <div id="jspsych-vsl-grid-scene-table-cell-1-1" style="width: 200px; height: 200px;"></div></td>
+    <td id="jspsych-vsl-grid-scene-table-1-2" style="padding: 20px 20px; border: 1px solid white;">
+    <div id="jspsych-vsl-grid-scene-table-cell-1-2" style="width: 200px; height: 200px;"></div></td>
+    <td id="jspsych-vsl-grid-scene-table-1-3" style="padding: 20px 20px; border: 1px solid white;">
+    <div id="jspsych-vsl-grid-scene-table-cell-1-3" style="width: 200px; height: 200px;"></div></td>
+    <td id="jspsych-vsl-grid-scene-table-1-4" style="padding: 20px 20px; border: 1px solid white;">
+    <div id="jspsych-vsl-grid-scene-table-cell-1-4" style="width: 200px; height: 200px;">
+    <img src="./images/right.png" style="width: 200px; height: 200"></img></div>
+    </td></tr><tr id="jspsych-vsl-grid-scene-table-row-2" css="height: 200px;">
+    <td id="jspsych-vsl-grid-scene-table-2-0" style="padding: 20px 20px; border: 1px solid white;">
+    <div id="jspsych-vsl-grid-scene-table-cell-2-0" style="width: 200px; height: 200px;"></div>
+    </td><td id="jspsych-vsl-grid-scene-table-2-1" style="padding: 20px 20px; border: 1px solid white;">
+    <div id="jspsych-vsl-grid-scene-table-cell-2-1" style="width: 200px; height: 200px;"></div>
+    </td><td id="jspsych-vsl-grid-scene-table-2-2" style="padding: 20px 20px; border: 1px solid white;">
+    <div id="jspsych-vsl-grid-scene-table-cell-2-2" style="width: 200px; height: 200px;"></div></td>
+    <td id="jspsych-vsl-grid-scene-table-2-3" style="padding: 20px 20px; border: 1px solid white;">
+    <div id="jspsych-vsl-grid-scene-table-cell-2-3" style="width: 200px; height: 200px;"></div></td>
+    <td id="jspsych-vsl-grid-scene-table-2-4" style="padding: 20px 20px; border: 1px solid white;">
+    <div id="jspsych-vsl-grid-scene-table-cell-2-4" style="width: 200px; height: 200px;"></div>
+    </td></tr></table></div>`
 
 // const PAIRED_ITEMS = [
 //     {list_name: LISTS[0], table: PAIRED_1},
