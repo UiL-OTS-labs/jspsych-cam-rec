0adeabb1b48753f9bc6a5c2537b5cb214536d145
Jacco van Elst <j.c.vanelst@uu.nl>
2021-05-17 22:26:21 +0200

    new screen wishes. the easy (but slightly annoying) stuff first....

--- jspsych/instructions.js
+++ jspsych/instructions.js
@@ -30,13 +30,36 @@ const GENERIC_CHECK = `
     toestemmingsverklaring.</p>
     `;
 
+const SCREEN_1 = `
+    <h3>Om te kunnen deelnemen aan het onderzoek is het volgende belangrijk:</h3>
+
+    <ol type="1">
+        <li>Voer het onderzoek uit op een <strong>laptop</strong> of <strong>desktop computer</strong>, <i>niet</i> op een telefoon of tablet.</li>
+        <li>Zorg ervoor dat uw <strong>toetsenbord</strong> werkt.</li>
+        <li>Zorg ervoor dat u een werkende <strong>muis</strong> of touchpad heeft.</li>
+        <li>Zorg ervoor dat het <strong>geluid</strong> in uw browser werkt en dat uw volume aan staat.</li>
+        <li>Gebruik een beveiligd <strong>thuis</strong> (wifi) <strong>netwerk</strong>.</li>
+    </ol>
+
+    <h3>Maximaliseer uw browserscherm voordat u verder gaat!</h3>
+    U krijgt eerst instructies hoe één en ander klaargezet moet en ingesteld moet worden. Pas als alles klaar staat krijgt u het verzoek om uw kind voor het scherm te plaatsen. 
+    <br>
+    <br>
+    Klik op 'volgende' als u aan de bovenstaande punten voldoet, en verder wilt gaan naar de toestemmingsverklaring.
+    `;
+
+// const SCREEN_2 etc. can be found in `consent.js`
+
 const PERSONAL_CODE_PRE_TEXT = `
     <h2>Persoonlijke code</h2>
     <p>Voer hier uw persoonlijke code in die u heeft ontvangen in de mail.
     <br>
     <br>
-    Het is belangrijk dat u dit correct invoert, zodat u ook achteraf kunt beslissen om toch niet mee te willen 
-    doen aan het onderzoek, en uw webcam-opname verwijderd kan worden.</p>
+    Het is belangrijk dat u de persoonlijke code correct invoert, zodat u ook achteraf kunt beslissen om toch niet mee te doen aan het onderzoek.
+    <br>
+    <br>
+    Alleen met een correcte persoonlijke code kunnen wij uw webcam-opname op uw verzoek verwijderen. Bewaar uw persoonlijke code dus goed <strong>*</strong>.
+    </p>
     `;
 
 const PERSONAL_CODE_TEXT = `
@@ -55,94 +78,65 @@ const PERSONAL_CODE_CHOICE_HTML =`
     `
 
 const PERSONAL_CODE_POST_TEXT = `
-    Klik op ‘volgende’ als u aan de persoonlijke code correct heeft ingevuld, en verder wilt gaan naar de opstelling instructies.
+    *) <sub>Zie informatiebrief voor meer informatie.</sub>
+    <br>
+    <br>
     `;
 
-// HTML plugin survey block: questions are in the HTML constant
-let personal_code_multi_html_block = {
-    type: 'survey-html-form',
-    preamble: CONSENT_HTML_STYLE_UU + PERSONAL_CODE_PRE_TEXT,
-    button_label: NEXT_BUTTON_TEXT,
-    html: PERSONAL_CODE_CHOICE_HTML,
-    on_finish: function(data){
-        var personal_code_html_responses = data.responses;
-        data.personal_code_response = personal_code_html_responses;
-    }
-};
-
-//audio test
-let test_audio_looped = {
-    post_trial_gap: DEFAULT_ITI,
-    timeline:
-        [
-            {
-                type: 'audio-button-response',
-                stimulus: './sounds/testtune.mp3',
-                choices: [PLAY_AGAIN_BUTTON_TEXT, NEXT_BUTTON_TEXT],
-                prompt: function(){
-                    return "<div class='instruction' >" + 
-                    '<img src="./images/speakersound.png" style="width: 100px; height: 100"></img>' +
-                    '<p>' + AUDIO_CHECK_PROMPT_TEXT_LOOP + '</p></div>'
-                }
-            }
-        ],
-    loop_function: function(data){
-        if (data.values()[0].button_pressed == 0){
-            return true; // loop again!
-        } else {
-            return false; // continue
-        }
-    },
-    on_finish: function(data) {
-        data.audio_checked = true;
-    }
-};
-
 const CAREGIVER_INSTRUCTION_PRE_IMAGE_TEXT = `
-    <h3>Idealiter ziet de opstelling van uw laptop of computer er uit zoals op deze afbeeldingen.</h3>
+    <h1> Instructies opstelling (1/2) </h1>
+    <h6>Idealiter ziet de opstelling van uw laptop of computer er uit zoals op deze afbeeldingen:</h6>
     `
 
 const CAREGIVER_INSTRUCTION_POST_IMAGE_TEXT_1 = `
-    <ul class="bullet-closed">
-    <li>De kinderstoel staat op ongeveer een (volwassen) armlengte afstand van het scherm, zodat er
-        ongeveer <strong>60 centimeter</strong> tussen uw kind en het scherm zit.</li> 
-    <li>De <i>webcam</i> is <strong>bovenin, in het midden van het scherm</strong> geplaatst.</li>
-    <li>De webcam is gericht op <strong>hoofdhoogte</strong> van uw kind. Indien uw kind hoger zit, plaats dan 
-        bijvoorbeeld enkele boeken onder de laptop, zodat de laptop op hoofdhoogte van uw kind staat.</li> 
-    <li>Er is <strong>voldoende (dag)licht</strong> in de omgeving zodat de ogen van uw kind goed zichtbaar zijn.
-    <ul class"bullet-open">
-        <li>Indien dit <i>niet</i> het geval is, plaatst u een <strong>lamp hoog achter het scherm</strong>, zodat 
-        er <strong>geen schaduw</strong> van uw scherm op het gezicht van uw kind valt.</li>
-    </ul>
-    </li>
+    <ol type="1">
+        <li> De kinderstoel staat op ongeveer een (volwassen) armlengte afstand van het scherm, zodat er ongeveer <strong>60 centimeter</strong> tussen uw kind en het scherm zit.</li>
+        <li> De webcam is <strong>bovenin, in het midden</strong> van het scherm geplaatst.</li>
+        <li> De webcam is gericht op <strong>hoofdhoogte</strong> van uw kind. Indien uw kind hoger zit, plaats dan bijvoorbeeld enkele boeken onder de laptop, zodat de webcam op hoofdhoogte van uw kind staat. </li>
+        '<img src="./images/screens.png" style="width: 400px; height: 100"></img>
+        <li> Er is <strong>voldoende (dag)licht</strong> in de omgeving zodat de ogen van uw kind goed zichtbaar zijn in de webcambeelden.</li>
+    </ol>
+    <div>
+        <ul class="bullet-open">
+            <li> Indien dit <i>niet</i> het geval is, plaatst u een <strong>lamp hoog achter het scherm</strong>, zodat er <strong>geen schaduw</strong> van uw scherm op het gezicht van uw kind valt.</li>
+            <li> Zorg ervoor dat er <strong>niet te veel licht <i>achter</i> uw kind</strong> vandaan komt om tegenlicht te voorkomen; zet uw kind dus idealiter niet met diens rug naar een raam.</li>
+        </ul>
+    </div>
     `;
 
 const CAREGIVER_INSTRUCTION_POST_IMAGE_TEXT_2 = `  
-    <ul class="bullet-closed">
-    <li>Zorg ervoor dat er niet te veel licht <i>achter</i> uw kind vandaan komt, om <strong>tegenlicht te voorkomen</strong>. 
-        Zet de kinderstoel dus idealiter niet voor een raam.</li>
-    <li>Zorg voor zo <strong>min mogelijk afleiding</strong>.</li>
-    <li>Zet <i>telefoon, tv, radio</i>, en <strong>alle andere apparatuur die kan storen</strong> <i>uit of op stil</i>, zodat
-        er gedurende het onderzoek <i>zo min mogelijk</i> auditieve of visuele <i>afleiding</i> is in de omgeving.</li> 
-    <li>Blijf <strong>achter uw kind</strong> staan of zitten en <i>kijk naar beneden</i> gedurende het onderzoek.
-    <ul class="bullet-open">
-        <li>Om het kijkgedrag van uw kind niet te beïnvloeden is het van belang dat u achter uw kind 
-            blijft, zodat uw kind maximale aandacht heeft voor het scherm en niet wordt afgeleid.</li>
-        <li>Tevens vragen wij van u om naar beneden te kijken (zodat u niet naar het scherm kijkt), 
-            <i>niet te praten over het scherm of wat u hoort, en niet te wijzen</i> naar het scherm.</li>
-    </ul>
-    </li>
-    </ul>
+    <h1> Instructies opstelling (2/2) </h1>
+    <ol type="1">
+        <li> Zorg voor zo <strong>min mogelijk afleiding</strong>. </li> 
+            <div>
+                <ul class="bullet-open">
+                    <li> Zet uw telefoon, tv, radio, en alle andere apparatuur die kan storen <strong>uit</strong> of op <strong>stil</strong>, zodat er gedurende het onderzoek zo min mogelijk geluiden of andere afleiding is in de omgeving.
+                </ul>
+            </div>
+        <li> Blijf <strong>zelf achter uw kind</strong> staan of zitten en kijk naar beneden gedurende het onderzoek.</li>
+            <div>
+                <ul class="bullet-open">
+                    <li>Om het kijkgedrag van uw kind niet te beïnvloeden is het van belang dat u <strong>achter</strong> uw kind blijft, zodat uw kind maximale aandacht heeft voor het scherm en niet wordt afgeleid.</li>
+                    <li>Verder vragen wij u om naar <strong>beneden</strong> te kijken (zodat u niet naar het scherm kijkt), <strong>niet</strong> te <strong>praten</strong> over het scherm of wat u hoort, en <strong>niet</strong> te <strong>wijzen</strong> naar het scherm.</li>
+                </ul>
+            </div>
+    </ol>
 `;
 
-const CAREGIVER_INSTRUCTION_SOOTHING = `<h2>Wanneer te stoppen?</h2>
-    Als uw kind erg afgeleid is kunt u uw kind geruststellen. Let wel op dat u niet met uw kind praat over of wijst naar 
-    het scherm. Als uw kind niet gerust te stellen is op een dergelijke wijze, dan kunt u het onderzoek stoppen.
-    `;
+const CAREGIVER_INSTRUCTION_SOOTHING = `
+    <h1>Wanneer te stoppen?</h1>
+    <p>Als uw kind erg onrustig is kunt u uw kind <strong>geruststellen</strong>. Let wel op dat u <strong>niet</strong> met uw kind <strong>praat</strong> over of <strong>wijst</strong> naar het <strong>scherm</strong>.
+    <br>
+    <br> 
+    <strong>Als uw kind niet gerust te stellen is, kunt u uw kind uit de kinderstoel halen.</strong>
+    <br>
+    <br>
+    <strong>Let op</strong>:  Wij vragen u de webcamopname te stoppen maar de opname <strong>wel</strong> te <strong>uploaden</strong>. Alle kleine beetjes helpen. Ook is het belangrijk om na het taakje op ‘volgende’ te blijven klikken en het <strong>onderzoek volledig af te ronden</strong>.
+    </p> 
+`;
 
 const CAREGIVER_VALIDATE_SETUP_STATEMENT = `
-    <strong>Ik heb de aanwijzingen over de opstelling zo goed mogelijk opgevolgd en begrijp wat er van mij verwacht wordt
-     tijdens het onderzoek.</strong>
+    Ik heb de aanwijzingen over de opstelling zo goed mogelijk opgevolgd en begrijp wat er van mij verwacht wordt tijdens het onderzoek.
      `;
 
 const SETUP_REFERENCE_NAME = "setup";
@@ -153,6 +147,9 @@ const IF_REQUIRED_FEEDBACK_MESSAGE_SETUP = `
 
 const AUDIO_CHECK_PROMPT_TEXT_LOOP = `
     Pas het test-geluid aan naar een volume die voor u en uw kind prettig is. U kunt het test-geluid opnieuw afspelen indien gewenst.
+    <br>
+    <br>
+    Zorg ervoor dat het geluid <strong>niet te zacht</strong> staat, anders wordt het geluid niet opgenomen door de webcam.
     <br>
     <br>
     Klik op ‘Volgende’ als uw geluid op een prettig volume staat, en u verder wilt gaan naar de lay-out van het scherm.
@@ -280,11 +277,12 @@ const GIFT_MESSAGE = `
     Klik op volgende als u dit heeft gedaan, of als u geen cadeautje wilt ontvangen.
     `;
 
+// don't mention closing the window in this frame, it may just leave you without data.
 const DEBRIEF_MESSAGE = `
     <h1>Einde</h1>
     <br>
     <br>
-    <h2>Bedankt voor jullie deelname! U kunt nu het scherm afsluiten</h2>
+    <h2>Bedankt voor jullie deelname!</h2>
     `;
 
-const DEBRIEF_MESSAGE_DURATION = 3500;
+const DEBRIEF_MESSAGE_DURATION = 1000;
