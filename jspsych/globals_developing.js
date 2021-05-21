c16e8be0000ef377d4068396f2fa9f1b8b74ea94
Jacco van Elst <j.c.vanelst@uu.nl>
2021-05-21 13:29:12 +0200

    protecting myself (and others)

---
+++ jspsych/globals_developing.js
@@ -0,0 +1,29 @@
+////////////////
+// GLOBALS
+///////////////
+
+// ACCESS_KEY needs to be used for server setup (data store)
+const ACCESS_KEY = '00000-0000000-000000-0000';
+
+// Defaults for buttons
+const NEXT_BUTTON_TEXT = "Volgende";
+const PREVIOUS_BUTTON_TEXT = "Vorige";
+const OK_BUTTON_TEXT = "Oké";
+const YES_BUTTON_TEXT = "Ja";
+const NO_BUTTON_TEXT = "Nee";
+const TRUE_BUTTON_TEXT = "Waar";
+const FALSE_BUTTON_TEXT = "Onwaar";
+const PLAY_AGAIN_BUTTON_TEXT = "Speel opnieuw af";
+
+const START_REAL_TASK_BUTTON_TEXT = NEXT_BUTTON_TEXT;
+
+// Default restrictions of minimal browser dimensions
+const MIN_WIDTH = 800; //will become wider...
+const MIN_HEIGHT = 600;
+
+// Bail out string for mobiles
+const BAIL_OUT_MOBILE_TEXT = "Dit experiment mag alleen worden uitgevoerd op een desktop of laptop!`"
+
+// Default ITI
+const DEFAULT_ITI = 500;
+
